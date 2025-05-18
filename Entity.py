3fb53c79b53929a2ddfc5ad80153c7c30fa0a578
Frytoslav <filipbujakowski1@gmail.com>
2025-05-18 20:42:48 +0200

    Added Enemy, Entity and Player classess

---
+++ Entity.py
@@ -0,0 +1,57 @@
+import abc
+from abc import abstractclassmethod, abstractmethod
+
+
+class Entity(abc.ABC):
+
+    def __init__(self, max_health, armor):
+        self._max_health = max_health
+        self._current_health = max_health
+        self._armor = armor
+
+    def lose_health(self, value):
+        if value < 0:
+            raise ValueError("Amount must be non-negative")
+        if value - self._armor < 0:
+            return
+        self._current_health -= value - self._armor
+
+    def gain_health(self, value):
+        if value < 0:
+            raise ValueError("Amount must be non-negative")
+        if self._current_health + value > self._max_health:
+            self._current_health = self._max_health
+        else:
+            self._current_health += value
+
+    @property
+    def max_health(self):
+        return self._max_health
+    @property
+    def current_health(self):
+        return self._current_health
+    @property
+    def armor(self):
+        return self._armor
+    @property
+    def is_alive(self):
+        return self._current_health > 0
+    @current_health.setter
+    def current_health(self, value):
+        if value < 0:
+            raise ValueError("Current health cannot be negative")
+        if value > self._max_health:
+            raise ValueError("Current health cannot exceed max health")
+        self._current_health = value
+
+    @max_health.setter
+    def max_health(self, value):
+        if value < 0:
+            raise ValueError("Max health cannot be negative")
+        self._max_health = value
+
+    @armor.setter
+    def armor(self, value):
+        if value < 0:
+            raise ValueError("Armor cannot be negative")
+        self._armor = value
---
+++ Player.py
@@ -0,0 +1,17 @@
+from Entity import Entity
+class Player(Entity):
+    """Class representing a player in the game."""
+    def __init__(self, name, max_health, armor):
+        super().__init__(max_health, armor)
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        self._name = value
+
