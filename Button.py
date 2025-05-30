86841c5abf5b9da0c7daada5e002248151e70f3e
Patryk Czekaj <patryk13290@gmail.com>
2025-05-30 21:40:49 +0200

    button optimization

--- Button.py
+++ Button.py
@@ -11,15 +11,16 @@ class Button(GameObject):
         self._width = width
         self._background_color = background_color
         self._position = position
-
-    def render(self, screen):
-        rect = pygame.Rect(
+        self.rect = pygame.Rect(
             self._position[0],
             self._position[1],
             self._width,
             self._height
         )
-        pygame.draw.rect(screen, self._background_color, rect)
+
+    def render(self, screen):
+        # self.rect.position = self.position
+        pygame.draw.rect(screen, self._background_color, self.rect)
 
 
 
