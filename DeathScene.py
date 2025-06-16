f2226b9fe9d2370b4defe3483b5b467456eaad6e
Frytoslav <filipbujakowski1@gmail.com>
2025-06-17 00:18:29 +0200

    Add background image to BattleScene and render it during gameplay

--- DeathScene.py
+++ DeathScene.py
@@ -19,10 +19,9 @@ class DeathScene(Scene):
             font_size=50,
             color=(255, 0, 0)
         )
-        battles_survived = globals.scene_manager.battle_index+1
         self.survived_text = Text(
             position=(globals.WIDTH // 2, globals.HEIGHT // 4 + 60),
-            text=f"You survived': {battles_survived} battles",
+            text=f"You survived': {globals.scene_manager.battle_index+1} battles",
             font_size=20,
             color=(255, 255, 255),
         )
